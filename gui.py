34c511a1db22edc207c7e0c95b7e01541de47279
Andrzej Janik <arkree@onet.pl>
2024-05-16 19:01:09 +0200

    Added image insertion via URL to classifier

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ import threading
 import queue
 import numpy as np
 from PIL import Image, ImageTk
+import requests
+import io
 from model import train_and_evaluate_model
 
 epoch_counter = 0  # Counter to keep track of epochs
@@ -37,6 +39,7 @@ def on_train():
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
+
 def on_classify():
     global model
     try:
@@ -54,19 +57,21 @@ def on_classify():
         img = np.expand_dims(img, axis=0)  # Adding batch dimension
 
         prediction = model.predict(img)  # Making prediction
-        result_label.config(text="Real" if prediction[0][0] < 0.5 else "Fake")  # Displaying result
-        prediction_label.config(text=f'Predicted value {prediction[0][0]:.4f}')
+        result_label.config(text="REAL" if prediction[0][0] < 0.5 else "FAKE")  # Displaying result
+        prediction_label.config(text=f'Predicted value: {prediction[0][0]:.4f}')
     except Exception as e:
         messagebox.showerror("Error", str(e))
+
 
 def train_model(conv_layers, activation, filters, kernel_size, dense_units, dropout_rate, epochs, batch_size, optimizer, loss, progress_queue):
     try:
         global model
         test_acc, model = train_and_evaluate_model(conv_layers, activation, filters, kernel_size, dense_units, dropout_rate, epochs, batch_size, optimizer, loss, progress_queue)  # Training the model
         messagebox.showinfo("Result", f'Test accuracy: {test_acc}')  # Displaying test accuracy
-        test_acc_label.config(text=f'Final model accuracy {test_acc:.4f}') # Displaying test accuracy
+        test_acc_label.config(text=f'Final model accuracy: {test_acc:.4f}') # Displaying test accuracy
     except Exception as e:
         messagebox.showerror("Error", str(e))
+
 
 def check_progress(progress_queue):
     global epoch_counter
@@ -80,10 +85,12 @@ def check_progress(progress_queue):
         pass
     root.after(100, check_progress, progress_queue)  # Scheduling next progress check
 
+
 def update_progress(iteration, logs):
     global epoch_counter
     progress_bar["value"] = iteration  # Updating progress bar value
     progress_label.config(text=f"Epoch: {epoch_counter}/{epochs_var.get()}, Loss: {logs['loss']:.4f}, Accuracy: {logs['accuracy']:.4f}")  # Updating progress label
+
 
 def browse_image():
     filepath = tk.filedialog.askopenfilename()  # Opening file dialog for image selection
@@ -91,12 +98,47 @@ def browse_image():
         image_path_var.set(filepath)  # Setting image path variable
         display_image(filepath)  # Displaying selected image
 
+
+def load_image_from_url(url):
+    response = requests.get(url, stream=True)  # Getting image from URL
+    response.raise_for_status()  # Checking for any errors in response
+    image_data = response.content  # Getting image content
+
+    # Opening image using PIL
+    img = Image.open(io.BytesIO(image_data))
+    img = img.resize((200, 200))  # Resizing image to fit in GUI
+
+    # Converting image to Tkinter format
+    img_tk = ImageTk.PhotoImage(img)
+
+    return img_tk
+
+
+def on_load_from_url():
+    try:
+        url = url_entry.get()  # Getting URL from entry
+        if not url:
+            raise ValueError("Please provide a URL.")
+
+        img_tk = load_image_from_url(url)  # Loading image from URL
+        image_label.img = img_tk  # Keeping reference to image to prevent garbage collection
+        image_label.configure(image=img_tk)  # Displaying image
+
+        # Clearing previous result
+        result_label.config(text="")
+        prediction_label.config(text="")
+
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
 def display_image(filepath):
     img = Image.open(filepath)  # Opening image
     img = img.resize((200, 200))  # Resizing image
     img = ImageTk.PhotoImage(img)  # Converting image to Tkinter format
     image_label.img = img   # Keeping reference to image to prevent garbage collection
     image_label.configure(image=img)  # Displaying image
+
 
 def update_filters_entries(*args):
     # Clear existing filter entries
@@ -117,6 +159,7 @@ def update_filters_entries(*args):
             entry.grid(row=i, column=1, sticky=(tk.W, tk.E), pady=2)
     except ValueError:
         pass  # Invalid input for number of layers
+
 
 # Creating GUI
 root = tk.Tk()  # Creating main window
@@ -192,46 +235,55 @@ conv_layers_var.trace_add("write", update_filters_entries)
 update_filters_entries()
 
 # Buttons
-train_button = ttk.Button(left_frame, text="Train", command=on_train)  # Creating train button
-train_button.grid(row=row+1, column=0, columnspan=2, pady=5, sticky=(tk.W, tk.E))  # Placing train button
+train_button = ttk.Button(left_frame, text="Train", command=on_train)
+train_button.grid(row=row+1, column=0, columnspan=2, pady=5, sticky=(tk.W, tk.E))
 
 # Progress bar
 progress_bar = ttk.Progressbar(left_frame, orient=tk.HORIZONTAL, length=200, mode='determinate')
-progress_bar.grid(row=row+2, column=0, columnspan=2, pady=5, sticky=(tk.W, tk.E))  # Placing progress bar
+progress_bar.grid(row=row+2, column=0, columnspan=2, pady=5, sticky=(tk.W, tk.E))
 
 # Progress label
-progress_label = ttk.Label(left_frame, text="Progress: 0%")
-progress_label.grid(row=row+3, column=0, columnspan=2, pady=5, sticky=(tk.W, tk.E))  # Placing progress label
+progress_label = ttk.Label(left_frame, text="")
+progress_label.grid(row=row+3, column=0, columnspan=2, pady=5, sticky=(tk.W, tk.E))
 
 # Test accuracy label
-test_acc_label = ttk.Label(left_frame, text="Final model accuracy: ")
+test_acc_label = ttk.Label(left_frame, text="")
 test_acc_label.grid(row=row+4, column=0, columnspan=3, pady=5, sticky=(tk.W, tk.E))
 
 row = 0
-# Image path
+# Image path from PC
 image_path_var = tk.StringVar()
-image_path_label = ttk.Label(right_frame, text="Image path:")
-image_path_label.grid(row=row, column=0, pady=5, sticky=tk.W)
 image_path_entry = ttk.Entry(right_frame, textvariable=image_path_var)
-image_path_entry.grid(row=row, column=1, pady=5, sticky=(tk.W, tk.E))
+image_path_entry.grid(row=row, column=0, columnspan=2, pady=5, padx=5, sticky=(tk.W, tk.E))
 browse_button = ttk.Button(right_frame, text="Browse", command=browse_image)
-browse_button.grid(row=row, column=2, pady=5, sticky=tk.W)
+browse_button.grid(row=row+1, column=0, columnspan=2, pady=5)
+
+# Input URL
+row += 1
+url_entry_var = tk.StringVar()
+url_entry = ttk.Entry(right_frame, textvariable=url_entry_var)
+url_entry.grid(row=row+2, column=0, columnspan=2, pady=5, padx=5, sticky=(tk.W, tk.E))
+
+# Button to load image from URL
+row += 1
+load_button = ttk.Button(right_frame, text="Load from URL", command=on_load_from_url)
+load_button.grid(row=row+3, column=0, columnspan=2, pady=5)
 
 # Right frame for displaying images and results
 image_label = ttk.Label(right_frame, text="")
-image_label.grid(row=row+1, column=1, padx=10, pady=10, sticky="nsew")
+image_label.grid(row=row+4, column=1, padx=10, pady=10, sticky="nsew")
 
 # Classify button
 classify_button = ttk.Button(right_frame, text="Classify", command=on_classify)
-classify_button.grid(row=row+2, column=0, columnspan=3, pady=5, sticky=(tk.W, tk.E))
+classify_button.grid(row=row+5, column=0, columnspan=3, pady=5, sticky=(tk.W, tk.E))
 
 
 # Result label
 result_label = ttk.Label(right_frame, text="")
-result_label.grid(row=row+3, column=0, padx=10, pady=10)
+result_label.grid(row=row+6, column=0, padx=10, pady=10)
 
 # Prediction label
 prediction_label = ttk.Label(right_frame, text="")
-prediction_label.grid(row=row+4, column=0, padx=10, pady=10)
+prediction_label.grid(row=row+7, column=0, padx=10, pady=10)
 
 root.mainloop()
